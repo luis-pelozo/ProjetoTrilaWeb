9e6f2ae5829d41e413a25a0eb9152bcb7e10a184
luis-pelozo <pelozoluis52@gmail.com>
2023-05-04 08:10:41 -0300

    Iniciado parte rest de busca de marcas no banco de dados

--- ProjetoTrilhaWeb/src/main/java/br/com/coldigogeladeiras/jdbcinterface/MarcaDAO.java
+++ ProjetoTrilhaWeb/src/main/java/br/com/coldigogeladeiras/jdbcinterface/MarcaDAO.java
@@ -1,58 +1,9 @@
 package br.com.coldigogeladeiras.jdbcinterface;
 
 import br.com.coldigogeladeiras.modelo.Marca;
-import jakarta.faces.application.StateManager;
-
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.Statement;
-import java.util.ArrayList;
 import java.util.List;
 
-public class MarcaDAO {
-    private Connection conexao;
-
-    public List<Marca>buscar(){
-
-        //Criação da instrução SQL para busca de todas as marcas
-        String comando = "SELECT * FROM marcas";
-
-        //Criação de uma lista para armazenar cada marca encontrada
-        List<Marca>listaMarcas = new ArrayList<Marca>();
-
-        //Criação do objeto marca com valor null(ou seja, sem instancia-lo)
-        Marca marca = null;
-
-        try{
-            //Uso da conexão do banco para prepara-lo para uma instrução SQL
-            Statement stmt = conexao.createStatement();
-
-            //Execução da instrução criada previamente
-            //e armazenamento do resultado no objeto rs
-            ResultSet rs = stmt.executeQuery(comando);
-
-            while (rs.next()){
-                marca = new Marca();
-
-                //Recebimento dos 2 dados retornados do BD  para cada linha encontrada
-                int id = rs.getInt("id");
-                String nome  = rs.getString("nome");
-
-                //Settando no objeto marca os valores encontrados
-                marca.setId(id);
-                marca.setNome(nome);
-
-                //Adição da instância contida no objeto Marca na Lista de marcas
-                listaMarcas.add(marca);
-            }
-
-        }catch (Exception ex){
-            //Caso alguma Exception não seja gerada no try, recebe-a no objeto "ex"
-            ex.printStackTrace();
-        }
-        //Retorna para quem chamou o metodo a lista criada
-        return listaMarcas;
-    }
-
+public interface MarcaDAO {
+    public List<Marca> buscar();
 }
 
--- ProjetoTrilhaWeb/src/main/java/br/com/coldigogeladeiras/rest/MarcaRest.java
+++ ProjetoTrilhaWeb/src/main/java/br/com/coldigogeladeiras/rest/MarcaRest.java
@@ -8,27 +8,35 @@ import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
-import jakarta.ws.rs.core.Response;
-import java.awt.*;
+
+import javax.ws.rs.core.Response;
 import java.sql.Connection;
 import java.util.ArrayList;
 import java.util.List;
 
 
 @Path("marca")
-public class MarcaRest {
+public class MarcaRest extends UtilRest {
     @GET
     @Path("/buscar")
     @Produces(MediaType.APPLICATION_JSON)
     public Response buscar(){
-        List<Marca>listaMarcas = new ArrayList<Marca>();
+        try {
 
-        Conexao conec = new Conexao();
-        Connection conexao = conec.abrirConexao();
+            List<Marca>listaMarcas = new ArrayList<Marca>();
 
-        JDBCMarcaDAO jdbcMarca = new JDBCMarcaDAO(conexao);
-        listaMarcas = jdbcMarca.buscar();
-        conec.fecharConexao();
-        return null;
+            Conexao conec = new Conexao();
+            Connection conexao = conec.abrirConexao();
+
+            JDBCMarcaDAO jdbcMarca = new JDBCMarcaDAO(conexao);
+            listaMarcas = jdbcMarca.buscar();
+            conec.fecharConexao();
+            return this.buildResponse(listaMarcas);
+
+        }catch (Exception e){
+            e.printStackTrace();
+            return this.buildErrorResponse(e.getMessage());
+        }
+
     }
 }
